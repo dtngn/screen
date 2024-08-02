7bd24139656c4855aef3c201a8a7ae16dca4c65b
Alexander Naumov <alexander_naumov@opensuse.org>
2024-08-02 17:54:16 +0200

    New command 'auth' for password protection
    Signed-off-by: Alexander Naumov <alexander_naumov@opensuse.org>

--- src/socket.c
+++ src/socket.c
@@ -831,7 +831,10 @@ void ReceiveMsg(void)
 	case MSG_ATTACH:
 		if (CreateTempDisplay(&m, recvfd, win))
 			break;
-		AskPassword(&m);
+		if (do_auth)
+			AskPassword(&m);
+		else
+			FinishAttach(&m);
 		break;
 	case MSG_ERROR:
 		{
@@ -850,7 +853,10 @@ void ReceiveMsg(void)
 	case MSG_POW_DETACH:
 		if (CreateTempDisplay(&m, recvfd, NULL))
 			break;
-		AskPassword(&m);
+		if (do_auth)
+			AskPassword(&m);
+		else
+			FinishAttach(&m);
 		break;
 	case MSG_QUERY:
 		{
